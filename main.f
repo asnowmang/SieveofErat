ff0d2ac0e814b48681e994adef28977200fd18b4
asnowmang <wsobs@yandex.com>
2018-07-04 02:58:13 -0500

    Switched to boolean variables

--- main.f
+++ main.f
@@ -1,8 +1,8 @@
 c     Sieve of Eratosthenes
 c     Find primes
       PROGRAM MAIN
+      LOGICAL line(99)
       INTEGER count, max
-      INTEGER line(99)
 
 c     Highest number to check for primeness
 c     Must equal line array size plus one
@@ -10,33 +10,33 @@ c     Must equal line array size plus one
       
 c     Populate array
       DO 10 count = 2, max
-         line(count) = count
+         line(count) = .TRUE.
  10   CONTINUE
 
 c     Cancel out multiples of two
       DO 20 count = 4, max, 2
-         line(count) = 0
+         line(count) = .FALSE.
  20   CONTINUE
 
 c     Cancel out multiples of three
       DO 30 count = 6, max, 3
-         line(count) = 0
+         line(count) = .FALSE.
  30   CONTINUE
 
-c     Cancel out multiples of 5
+c     Cancel out multiples of five
       DO 40 count = 10, max, 5
-         line(count) = 0
+         line(count) = .FALSE.
  40   CONTINUE
 
-c     Cancel out multiples of 7
+c     Cancel out multiples of seven
       DO 50 count = 14, max, 7
-         line(count) = 0
+         line(count) = .FALSE.
  50   CONTINUE
 
 c     Print all odds in array
       DO 60 count = 2, max
-         IF (line(count) .NE. 0) THEN
-            PRINT *, line(count)
+         IF (line(count) .EQV. .TRUE.) THEN
+            PRINT *, count
          ELSE
             CONTINUE
          ENDIF
